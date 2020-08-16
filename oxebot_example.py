ca8ef9ded6cace32be4409e502bf15ba480e4fa5
yzakius <isaacmob@gmail.com>
2020-08-16 20:53:21 -0300

    migrated to lib pyTelegramBotAPI
    closes #7

--- oxebot_example.py
+++ oxebot_example.py
@@ -1,35 +1,53 @@
 import random
-import telepot
-import time
+import telebot
 import requests
-# import json
- 
-def handle(msg):
-    chat_id = msg['chat']['id']
-    command = msg['text']
-    print(f'Comando recebido: {command}')
-    if "exemplo1" in command:
-        lines = open('general.txt').read().splitlines()
-        mensagem = random.choice(lines)
-        bot.sendMessage(chat_id, mensagem)
-    elif "exemplo2" in command:
-        bot.sendMessage(chat_id, "Você escreveu exemplo2")
-    elif command == "/tempo":
-        get_weather = requests.get(
-            "http://wttr.in/Recife?format=j1", headers={"user-agen": "curl"}
-            )
-        weather = get_weather.json()
-        temperature = weather['current_condition'][0]['temp_C']
-        weather_code = weather['current_condition'][0]['weatherCode']
-        if weather_code == "176":
-            weather_code = "🌧"
-        bot.sendMessage(
-            chat_id, f"A temperatura em Recife está {temperature} graus. {weather_code}"
-            )
+import json
+from decouple import config
 
-bot = telepot.Bot(config("telegram_key"))
-bot.message_loop(handle)
-print('Ahhhhhhhhhhhhh!!!')
-while True:
-    pass
-    time.sleep(1)
+bot = telebot.TeleBot(config("telegram_key"))
+
+@bot.message_handler(commands=["start", "help"])
+def send_help(message):
+	bot.reply_to(message, "Estamos em  🚧")
+
+@bot.message_handler(commands=["tempo"])
+def wetaher(message):
+    get_weather = requests.get(
+        "http://wttr.in/Recife?format=j1", headers={"user-agen": "curl"}
+    )
+    weather = get_weather.json()
+    temperature = weather['current_condition'][0]['temp_C']
+    weather_code = weather['current_condition'][0]['weatherCode']
+    if weather_code == "176":
+         weather_code = "🌧"
+    elif weather_code == "227":
+        weather_code = "⛄"
+    else:
+        weather_code = "🤔"
+    bot.reply_to(
+        message, f"A temperatura em Recife está {temperature} graus. {weather_code}"
+    )
+
+@bot.message_handler(content_types = ["text"])
+def read_words(message):
+    text = message.text.lower()
+    print(f"[{message.from_user.first_name}]: {text}")
+    chat_id = message.chat.id
+    if "acho" in text:
+        bot.send_message(chat_id, "Você acha ou tem certeza?")
+    elif "mas" in text:
+        lines = open("general.txt").read().splitlines()
+        oxebot_message = random.choice(lines)
+        bot.send_message(chat_id, oxebot_message)
+    elif "yzakius" in text:
+        lines = open("yzakius.txt").read().splitlines()
+        oxebot_message = random.choice(lines)
+        bot.send_message(chat_id, oxebot_message)
+    elif "hehe" in text:
+        bot.send_message(chat_id, "Putz. Olha só essa risada kkk")
+
+bot.polling()
+
+#while True:
+#    pass
+#    time.sleep(1)
