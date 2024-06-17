bc121487214a502cb4daf3b8fc880714efc9bb52
Isaac Ferreira Filho <dev@yzakius.me>
2024-06-16 21:48:05 -0300

    Add black and apply
    - Add black
    - Aplly code format
    
    Closes #34

--- src/oxebot/oxebot.py
+++ src/oxebot/oxebot.py
@@ -5,15 +5,15 @@ import time
 from decouple import config
 from utils import can_talk
 
-bot = telebot.TeleBot(config('telegram_key'))
+bot = telebot.TeleBot(config("telegram_key"))
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=["start", "help"])
 def send_help(message):
-    bot.reply_to(message, 'Estamos em  🚧')
+    bot.reply_to(message, "Estamos em  🚧")
 
 
-@bot.message_handler(commands=['tempo'])
+@bot.message_handler(commands=["tempo"])
 def weather(message):
     wwo_code = {
         "113": "Sunny",
@@ -87,106 +87,103 @@ def weather(message):
         "VeryCloudy": "☁️",
     }
 
-    r = requests.get(
-        'https://wttr.in/Recife?format=j1', headers={'User-Agent': 'curl'}
-    )
+    r = requests.get("https://wttr.in/Recife?format=j1", headers={"User-Agent": "curl"})
 
     if r.status_code == 200:
         r_json = r.json()
-        current_condition = r_json.get('current_condition')
+        current_condition = r_json.get("current_condition")
 
         if isinstance(current_condition, list) and len(current_condition):
-            temperature = current_condition[0]['temp_C']
-            weather_code = current_condition[0]['weatherCode']
+            temperature = current_condition[0]["temp_C"]
+            weather_code = current_condition[0]["weatherCode"]
 
             weather_icon = weather_symbol.get(
-                wwo_code.get(weather_code),
-                weather_symbol['Unknown']
+                wwo_code.get(weather_code), weather_symbol["Unknown"]
             )
 
             bot.reply_to(
-                message, 'A temperatura em Recife está '
-                         f'{temperature} graus. {weather_icon}'
+                message,
+                "A temperatura em Recife está " f"{temperature} graus. {weather_icon}",
             )
 
             return
 
-    bot.reply_to(message, 'Erro ao buscar informações')
+    bot.reply_to(message, "Erro ao buscar informações")
 
 
-@bot.message_handler(commands=['cotacao'])
+@bot.message_handler(commands=["cotacao"])
 def cotation(message):
     get_cotation = requests.get(
-        'https://economia.awesomeapi.com.br/json/all',
-        headers={'user-agen': 'curl'}
+        "https://economia.awesomeapi.com.br/json/all", headers={"user-agen": "curl"}
     )
     cota = get_cotation.json()
-    dolar = float(cota['USD']['high'])
-    euro = float(cota['EUR']['high'])
-    libra = float(cota['GBP']['high'])
+    dolar = float(cota["USD"]["high"])
+    euro = float(cota["EUR"]["high"])
+    libra = float(cota["GBP"]["high"])
     bot.reply_to(
-        message, f'================================= \n '
-                 f'O dólar está custando: R$ {dolar:.2f},\n '
-                 f'O euro está custando: R$ {euro:.2f}, \n '
-                 f'A libra está custando: R$ {libra:.2f}'
+        message,
+        f"================================= \n "
+        f"O dólar está custando: R$ {dolar:.2f},\n "
+        f"O euro está custando: R$ {euro:.2f}, \n "
+        f"A libra está custando: R$ {libra:.2f}",
     )
 
 
-@bot.message_handler(commands=['quote_add'])
+@bot.message_handler(commands=["quote_add"])
 def quote_add(message):
     text = message.text
     chat_id = message.chat.id
     if len(text) > 10:
-        text_replaced = text.replace('/quote_add ', '')
-        quote = open('assets/text/quotes.txt', 'a')
-        quote.write(f'\n{text_replaced}')
+        text_replaced = text.replace("/quote_add ", "")
+        quote = open("assets/text/quotes.txt", "a")
+        quote.write(f"\n{text_replaced}")
         quote.close()
-        bot.send_message(chat_id, 'Arquivado com sucesso ;)')
+        bot.send_message(chat_id, "Arquivado com sucesso ;)")
     else:
-        bot.send_message(chat_id, 'Leia o MANUAL!!!!!!')
+        bot.send_message(chat_id, "Leia o MANUAL!!!!!!")
 
 
-@bot.message_handler(commands=['quote'])
+@bot.message_handler(commands=["quote"])
 def quote(message):
     chat_id = message.chat.id
-    lines = open('assets/text/quotes.txt').read().splitlines()
+    lines = open("assets/text/quotes.txt").read().splitlines()
     oxebot_message = random.choice(lines)
     bot.send_message(
         chat_id,
-        f'=========================== \n'
-        f'Momento de Sabedoria no OXE: \n'
-        f'=========================== \n\n'
-        f'{oxebot_message}'
+        f"=========================== \n"
+        f"Momento de Sabedoria no OXE: \n"
+        f"=========================== \n\n"
+        f"{oxebot_message}",
     )
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(content_types=["text"])
 def read_words(message):
     text = message.text.lower()
-    print(f'[{message.from_user.first_name}]: {text}')
+    print(f"[{message.from_user.first_name}]: {text}")
     msg_time = message.date
     current_time = int(time.time())
     msg_tolerance_time = current_time - msg_time
     if msg_tolerance_time <= 3:
         chat_id = message.chat.id
-        if 'acho' in text:
+        if "acho" in text:
             if can_talk():
-                lines = open('assets/text/acho.txt').read().splitlines()
+                lines = open("assets/text/acho.txt").read().splitlines()
                 oxebot_message = random.choice(lines)
                 bot.send_message(chat_id, oxebot_message)
         elif "mas" in text:
             if can_talk():
-                lines = open('assets/text/general.txt').read().splitlines()
+                lines = open("assets/text/general.txt").read().splitlines()
                 oxebot_message = random.choice(lines)
                 bot.send_message(chat_id, oxebot_message)
-        elif 'yzakius' in text:
+        elif "yzakius" in text:
             if can_talk():
-                lines = open('assets/text/yzakius.txt').read().splitlines()
+                lines = open("assets/text/yzakius.txt").read().splitlines()
                 oxebot_message = random.choice(lines)
                 bot.send_message(chat_id, oxebot_message)
-        elif 'hehe' in text:
+        elif "hehe" in text:
             if can_talk():
-                lines = open('assets/text/risada.txt').read().splitlines()
+                lines = open("assets/text/risada.txt").read().splitlines()
                 oxebot_message = random.choice(lines)
                 bot.send_message(chat_id, oxebot_message)
 
